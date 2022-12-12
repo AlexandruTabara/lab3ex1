ac2b24103b2b6aeefc636f4081176a9fd5a82124
Alexandru Tabara <alexandru.tab1@gmail.com>
2022-12-12 12:53:55 +0200

    Add project files.

---
+++ lab3ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab3ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex 1
+            //Scrieti o functie care va calcula suma cifrelor unui numar.
+
+            int adunare01 = Adunare(6, 4);
+
+            static int Adunare(int x, int y)
+            {
+                return x + y;
+            }
+
+            Console.WriteLine(adunare01);
+        }
+    }
+}
